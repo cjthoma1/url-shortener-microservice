f05a8826102924638f2fb7c2dd9bbee458fab687
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-06-01 12:00:32 +0000

    Passing valid url as a url parameter

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require("express"),
 app = express(),
 path = require('path'),
-port = process.env.PORT || '8080'
+port = process.env.PORT || '8080',
+
+validUrl = require("valid-url"),
+paramUrl = '';
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+app.use("/new/:url", (req, res) => {
+    paramUrl = req.params.url +"/"+req.path
+   if(validUrl.isUri(paramUrl)) {
+     res.redirect(paramUrl);
+   }
+   else{
+       res.send("Whoops")
+   }
+})
+
+app.get("/:id", (req, res) => {
+    if(parseInt(req.params.id)){
+        res.send("ID for "+ paramUrl)
+    }
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("Listening on port "+port);
